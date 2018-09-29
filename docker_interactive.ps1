9c3d82e9daf8a7bfd176077335c64eafb0ae8af8
sumbleen <42788130+sumbleen@users.noreply.github.com>
2018-09-28 20:32:23 -0400

    First

--- docker_interactive.ps1
+++ docker_interactive.ps1
@@ -1,5 +1,9 @@
-﻿$myname=hostname;
-$IP=test-connection "$myname" -count 1;
+$sumbleen=hostname;
+
+$IP=test-connection "$sumbleen" -count 1;
+
 $myip=$IP.ipv4address.IPAddressToString
+
 set-variable -name DISPLAY -value ${myip}:0.0
+
 docker run -it --rm -e DISPLAY=$DISPLAY $dockerargs rhancock/burc-lite /bin/bash
